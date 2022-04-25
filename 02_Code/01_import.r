8b64d3d981f93588d1085ce44dfd8fd031158314
Samuel Aeschbach <samuel.aeschbach@unibas.ch>
2022-04-25 15:26:04 +0200

    add example data

---
+++ 02_Code/01_import.r
@@ -0,0 +1,9 @@
+# dependencies -----------------------------------------------------------------
+
+library(tidyverse)
+
+# import data from filesystem --------------------------------------------------
+
+mtcars <- read_csv("01_Data/data.csv")
+
+# do something
